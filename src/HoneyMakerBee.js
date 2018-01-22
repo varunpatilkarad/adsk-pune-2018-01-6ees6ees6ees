1f09f7373f466b9a5d3efdac160b014c9c5371f9
Varun Patil <patilv@PUNC02T12L2H03Q.local>
2018-01-22 12:01:19 +0530

    indent

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,18 +1,14 @@
 class HoneyMakerBee extends Bee {
-
   constructor(honeyPot = 0)
   {
     super("make honey");
     this.age = 10;
     this.honeyPot = honeyPot;
   }
-
   makeHoney(){
     this.honeyPot += 1;
   };
-
   giveHoney(){
     this.honeyPot -= 1;
   }
-
 };
